32e8b08d8d7abad4afb0798f60cb08159b9f4b0b
Denis Shibidzhiev <dshibidzhiev@ubic.tech>
2021-06-15 01:18:53 +0300

    add main component with mockData

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Main from '../main/main';
 
-function App() {
-  return <Main />;
+const propTypes = {
+  places: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      price: PropTypes.number,
+      rating: PropTypes.number,
+      type: PropTypes.string,
+      favorite: PropTypes.bool,
+      premium: PropTypes.bool,
+    }),
+  ).isRequired,
+};
+
+function App({ places }) {
+  return <Main places={places} />;
 }
+
+App.propTypes = propTypes;
 
 export default App;
--- project/src/components/app/app.test.js
+++ project/src/components/app/app.test.js
@@ -4,6 +4,6 @@ import App from './app';
 
 test('Renders app-component', () => {
   render(<App />);
-  const textElement = screen.getByText(/Hello, world!/i);
+  const textElement = screen.getByText(/Canal View Prinsengracht/i);
   expect(textElement).toBeInTheDocument();
 });
