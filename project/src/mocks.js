e5768452a95b959c5b3387c0a2e6d3dd3b00fce1
Denis Shibidzhiev <dshibidzhiev@ubic.tech>
2021-06-15 01:20:24 +0300

    @/src/index.js: add mocksData

---
+++ project/src/mocks.js
@@ -0,0 +1,42 @@
+export const mockArray = [
+  {
+    title: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartment',
+    price: 120,
+    rating: 4,
+    favorite: false,
+    premium: true,
+  },
+  {
+    title: 'Wood and stone place',
+    type: 'Private room',
+    price: 80,
+    rating: 4,
+    favorite: true,
+    premium: false,
+  },
+  {
+    title: 'Canal View Prinsengracht',
+    type: 'Apartment',
+    price: 132,
+    rating: 4,
+    favorite: false,
+    premium: false,
+  },
+  {
+    title: 'Nice, cozy, warm big bed apartment',
+    type: 'Apartment',
+    price: 180,
+    rating: 5,
+    favorite: false,
+    premium: true,
+  },
+  {
+    title: 'Wood wood weed place',
+    price: 1000,
+    rating: 5,
+    type: 'Cozy',
+    favorite: true,
+    premium: false,
+  },
+];
